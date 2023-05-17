59ecec5ffc5be167ad9691ea63fbc1ee26211221
jgnacio <jgnaciogomez@gmail.com>
2023-05-17 15:14:28 -0300

    Update: connect the server with the client

--- client/pages/api/hello.js
+++ client/pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const axios = require("axios");
 
 export default function hello(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  let buffer = "";
+  axios
+    .get("http://api:3005/")
+    .then(async function (response) {
+      buffer = await response;
+    })
+    .catch(function (error) {
+      buffer = response;
+      console.log(error);
+    });
+  console.log(typeof buffer);
+  console.log(buffer);
+  res.status(200).json({ name: buffer });
 }
